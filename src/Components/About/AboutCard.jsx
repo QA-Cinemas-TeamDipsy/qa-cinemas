29c95de1551ef31aa54e68a837180a8cc93e9047
zaynejamilQA <zjamil@qa.com>
2021-06-02 14:26:06 +0100

    QAC-18 #comment Added about and about card files Co-authored-by: Usman Sajid <usmansafk@users.noreply.github.com> Co-authored-by: philipsephton-qa <philipsephton-qa@users.noreply.github.com> Co-authored-by: LamarrRD-QA <LamarrRD-QA@users.noreply.github.com> Co-authored-by: NeelDQA <NeelDQA@users.noreply.github.com>

--- src/Components/About/AboutCard.jsx
+++ src/Components/About/AboutCard.jsx
@@ -1,23 +1,25 @@
-import {Card, Button} from 'react-bootstrap'
+import {Card,ListGroup,ListGroupItem,CardSubtitle} from 'react-bootstrap'
 
 const AboutCard = (props) => {
-    const { name, age, jobTitle, Hobbies, email, img } = props.data;
+    const { name, surname ,age, jobTitle, hobbies, email, img, twitter } = props.data;
 
 
 console.log(props);
     return (
-        <Card style={{ width: '18rem' }}>
+        <Card className = "mb-10" style={{background:'orange', width: '18 rem' }}>
             <Card.Img variant="top" src={img} />
             <Card.Body>
                 <Card.Title>{name}</Card.Title>
+                <Card.Title>{surname}</Card.Title>
                 <Card.Subtitle className="mb-2 text-muted">{jobTitle}, {age}</Card.Subtitle>
-                <Card.Text>
-                    obbies:{Hobbies}"
-                    Email:{email}
-                    
-                  
-                </Card.Text>
-                <Button variant="primary">Go somewhere</Button>
+
+             <ListGroup >
+                <ListGroup.Item variant = "warning"> Hobbies: {hobbies}</ListGroup.Item>
+                <ListGroup.Item variant = "warning">Email: {email}</ListGroup.Item>
+                <ListGroup.Item variant = "warning">Twitter: {twitter}</ListGroup.Item>
+                
+            </ListGroup>
+                
             </Card.Body>
         </Card>
 
--- src/Components/About/AboutPage.jsx
+++ src/Components/About/AboutPage.jsx
@@ -1,15 +1,28 @@
 import details from "../static/TeamDetails.json"
 import AboutCard from "./AboutCard"
+import { CardDeck, Container } from "react-bootstrap"
 
 const About = () => {
     return (
         <>
-            <h1>About</h1>
-            {
-                details.map((data, id) => (
-                    <AboutCard key={id} data={data} />
-                ))
-            }
+        <div className="text-center">
+            <h1>About Us</h1>
+        </div>
+            <Container>
+                <CardDeck>
+                {
+
+                    details.map((data, id) => (
+
+                        
+                            <AboutCard key={id} data={data} />
+                         ))
+
+                }
+                </CardDeck>
+
+            </Container>
+            
 
         </>
     )
