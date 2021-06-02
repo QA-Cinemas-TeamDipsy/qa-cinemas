460a234ef601bbd057c373f6bc0da23a580c69cd
zaynejamilQA <zjamil@qa.com>
2021-06-02 14:24:58 +0100

    QAC-1 #comment Updated NavigationBar added routing Co-authored-by: Usman Sajid <usmansafk@users.noreply.github.com> Co-authored-by: philipsephton-qa <philipsephton-qa@users.noreply.github.com> Co-authored-by: LamarrRD-QA <LamarrRD-QA@users.noreply.github.com> Co-authored-by: NeelDQA <NeelDQA@users.noreply.github.com>

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
 import HomePage from "./Components/HomePage";
+import AboutPage from "./Components/About/AboutPage"
+import NavigationBar from "./Components/NavigationBar/NavigationBar"
+import ContactUs from './Components/ContactUs/ContactUs'
+import Footer from "./Components/Footer/Footer"
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
+
 import "./App.css";
 
 function App() {
   return (
-    <>
-      <HomePage />
-    </>
+
+    <Router>
+      <NavigationBar />
+
+      <Switch>
+        <Route exact path="/">
+          <HomePage />
+        </Route>
+        <Route path="/ContactUs" component={ContactUs} />
+        <Route path="/about">
+          <AboutPage />
+        </Route>
+      </Switch>
+    
+      <Footer />
+    </Router>
+
+
+
   );
 }
 
--- src/Components/NavigationBar/NavigationBar.jsx
+++ src/Components/NavigationBar/NavigationBar.jsx
@@ -6,29 +6,36 @@ import {
   Button,
   Form,
   FormControl,
+  Image
 } from "react-bootstrap";
+import { NavLink } from 'react-router-dom';
+import logo from "../static/Logo.png";
 
 const NavigationBar = () => {
   return (
     <>
-      <div>
-        <Navbar className="nav-top">
-          <Navbar.Brand href="#home">QA Cinema</Navbar.Brand>
+      <div className="nav-top">
+         
+        <Navbar sticky="top" >
+          <Navbar.Brand to="/" href="/">
+            <Image src={logo} width={150} />
+          </Navbar.Brand>
           <Button variant="danger">Login</Button>
           <Button variant="danger">Sign Up</Button>
         </Navbar>
 
-        <Navbar
+
+        <Navbar sticky="top" 
           className="nav-bottom"
           bg="dark"
           variant="dark"
           max-width="100%"
         >
           <Nav className="mr-auto">
-            <Nav.Link href="#movies">What's On</Nav.Link>
-            <Nav.Link href="#cinemas">Cinemas</Nav.Link>
-            <Nav.Link href="#pricing">Contact Us</Nav.Link>
-            <Nav.Link href="#about">About Us</Nav.Link>
+            <Nav.Link href="/movies">What's On</Nav.Link>
+            <Nav.Link href="/cinemas">Cinemas</Nav.Link>
+            <Nav.Link href="/contactus">Contact Us</Nav.Link>
+            <Nav.Link to="/about" href="/about">About Us</Nav.Link>
           </Nav>
 
           <Form inline>
