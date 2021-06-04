b392792ef6f04ee7d70b06e5da0eddbab140ae59
Philip Sephton <psephton@academytrainee.com>
2021-06-04 10:49:43 +0100

    QAC-26 #comment Added images with alt

---
+++ src/Components/Classifications/Classifications.jsx
@@ -0,0 +1,17 @@
+
+
+const Classifications = () = => {
+
+
+    return(
+<>
+        <img src="https://static.wikia.nocookie.net/rating-system/images/8/88/BBFC_U.svg/revision/latest/scale-to-width-down/120?cb=20201128032256" alt="U" />
+        <img src="https://static.wikia.nocookie.net/rating-system/images/b/b5/BBFC_PG.svg/revision/latest/scale-to-width-down/120?cb=20201128032358" alt="PG" />
+        <img src="https://static.wikia.nocookie.net/rating-system/images/8/87/BBFC_12A.svg/revision/latest/scale-to-width-down/120?cb=20201128033054" alt="12A" />
+        <img src="https://static.wikia.nocookie.net/rating-system/images/0/03/BBFC_15.svg/revision/latest/scale-to-width-down/120?cb=20201128032658" alt="15" />
+        <img src="https://static.wikia.nocookie.net/rating-system/images/9/9f/BBFC_18.svg/revision/latest/scale-to-width-down/120?cb=20201128155533" alt="18" />
+        </>
+    )
+}
+
+export default Classifications;
