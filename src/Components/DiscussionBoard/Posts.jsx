256f0a890183411b027da626003aa8b39237736e
Usman Sajid <usman230@hotmail.co.uk>
2021-06-07 00:38:37 +0100

    QAC-17 #comment US:Updateed Posts Component to display created posts in UI

--- src/Components/DiscussionBoard/Posts.jsx
+++ src/Components/DiscussionBoard/Posts.jsx
@@ -1,5 +1,34 @@
-const Posts = () => {
-  return <></>;
+import { Button, Form, Row, Container, Col } from "react-bootstrap";
+
+const Posts = ({ posts }) => {
+  const displayRating = (rating) => {
+    rating = parseInt(rating);
+    return Array(rating).fill("★");
+  };
+
+  return (
+    <>
+      <h1>Discussion Board</h1>
+
+      <Container>
+        {posts.length > 0 ? (
+          posts.map((post) => (
+            <>
+              ⊱――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――⊰
+              <h2>[user-name]: {post.title}</h2>
+              <h3>Movie: {post.movie}</h3>
+              <h3>Rating: {displayRating(post.rating)}</h3>
+              <h4>Comment: {post.comment}</h4>
+            </>
+          ))
+        ) : (
+          <h3>
+            <em>No Posts Yet!</em>
+          </h3>
+        )}
+      </Container>
+    </>
+  );
 };
 
 export default Posts;
