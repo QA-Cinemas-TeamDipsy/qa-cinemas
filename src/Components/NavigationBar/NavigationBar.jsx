d7a1ebd0fc9f6923f8a8b90d580a70dc150ccc53
Usman Sajid <usman230@hotmail.co.uk>
2021-06-06 22:08:54 +0100

    QAC-1 #comment US: updated nav bar logos

--- src/Components/NavigationBar/NavigationBar.jsx
+++ src/Components/NavigationBar/NavigationBar.jsx
@@ -7,19 +7,21 @@ import {
   Button,
   Form,
   FormControl,
-  Image
+  Image,
 } from "react-bootstrap";
-import { NavLink } from 'react-router-dom';
+import { NavLink } from "react-router-dom";
 import logo from "../static/Logo.png";
-
 
 const NavigationBar = () => {
   return (
     <>
-
-      <Navbar className="nav-top" sticky="top" bg="dark"
+      <Navbar
+        className="nav-top"
+        sticky="top"
+        bg="dark"
         variant="dark"
-        expand="lg">
+        expand="lg"
+      >
         <Navbar.Brand to="/" href="/">
           <Image src={logo} width={150} />
         </Navbar.Brand>
@@ -28,23 +30,39 @@ const NavigationBar = () => {
           <Nav.Link href="/movies">What's On</Nav.Link>
           <Nav.Link href="/cinemas">Cinemas</Nav.Link>
           <Nav.Link href="/contactus">Contact Us</Nav.Link>
-          <Nav.Link to="/about" href="/about">About Us</Nav.Link>
+          <Nav.Link to="/about" href="/about">
+            About Us
+          </Nav.Link>
         </Nav>
 
         <Nav className="mr-3">
-          <Button variant="danger" className="rounded-pill" style={{borderColor:"#343A40", border: "10px"}}>Login</Button>{' '}
-          <Button variant="danger" className="rounded-pill" style={{borderColor:"#343A40", border: "10px"}}>Sign Up</Button>
+          <Button
+            variant="danger"
+            className="rounded-pill"
+            style={{ borderColor: "#343A40", border: "10px" }}
+          >
+            {" "}
+            <img src="https://img.icons8.com/android/24/000000/login-rounded-right.png" />
+            Login
+          </Button>{" "}
+          <Button
+            variant="danger"
+            className="rounded-pill"
+            style={{ borderColor: "#343A40", border: "10px" }}
+          >
+            <img src="https://img.icons8.com/small/32/000000/user.png" />
+            Sign Up
+          </Button>
         </Nav>
 
         <Nav>
           <Form inline>
-            {' '}
+            {" "}
             <FormControl type="text" placeholder="Search" className="mr-sm-2" />
             <Button variant="outline-danger">Search</Button>
           </Form>
         </Nav>
       </Navbar>
-
     </>
   );
 };
