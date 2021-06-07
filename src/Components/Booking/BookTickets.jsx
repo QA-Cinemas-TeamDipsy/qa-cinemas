f3cd385f8ca3906122466e649b6b67d1164d58b5
Philip Sephton <psephton@academytrainee.com>
2021-06-07 11:47:43 +0100

    QAC-14 #comment Added senior

--- src/Components/Booking/BookTickets.jsx
+++ src/Components/Booking/BookTickets.jsx
@@ -1,74 +1,113 @@
 import { useState } from "react";
-import PaymentForm from "./PaymentForm";
 
+const BookTickets = ({ adultTicketPrice, childTicketPrice, seniorTicketPrice }) => {
+  const [adultTickets, setAdultTickets] = useState("");
+  const [adultTicketsCost, setAdultTicketsCost] = useState("");
+  const [childTickets, setChildTickets] = useState("");
+  const [childTicketsCost, setChildTicketsCost] = useState("");
+  const [seniorTickets, setSeniorTickets] = useState("");
+  const [seniorTicketsCost, setSeniorTicketsCost] = useState("");
+  const [totalTickets, setTotalTickets] = useState("");
+  const [totalTicketsPrice, setTotalTicketsPrice] = useState("");
 
-const BookTickets = ({ adultTicketPrice, childTicketPrice, movieTitle, firstName, time, date, rating }) => {
-    const [adultTickets, setAdultTickets] = useState("");
-    const [adultTicketsCost, setAdultTicketsCost] = useState("");
-    const [childTickets, setChildTickets] = useState("");
-    const [childTicketsCost, setChildTicketsCost] = useState("");
-    const [totalTickets, setTotalTickets] = useState("")
-    const [totalTicketsPrice, setTotalTicketsPrice] = useState("")
+  const adultTicketUpdate = (e) => {
+    setTotalTickets(adultTickets + childTickets + seniorTickets);
+    setAdultTickets(e.target.value);
+    setAdultTicketsCost(e.target.value * adultTicketPrice);
+    setTotalTicketsPrice(
+      adultTicketsCost + childTicketsCost + seniorTicketsCost
+    );
+  };
+  const childTicketUpdate = (e) => {
+    setTotalTickets(adultTickets + childTickets + seniorTickets);
+    setChildTickets(e.target.value);
+    setChildTicketsCost(e.target.value * childTicketPrice);
+    setTotalTicketsPrice(
+      adultTicketsCost + childTicketsCost + seniorTicketsCost
+    );
+  };
+  const seniorTicketUpdate = (e) => {
+    setTotalTickets(adultTickets + childTickets + seniorTickets);
+    setSeniorTickets(e.target.value);
+    setSeniorTicketsCost(e.target.value * seniorTicketPrice);
+    setTotalTicketsPrice(
+      adultTicketsCost + childTicketsCost + seniorTicketsCost
+    );
+  };
 
-    const adultTicketUpdate = (e) => {
-        setTotalTickets(adultTickets + childTickets);
-        setAdultTickets(e.target.value);
-        setAdultTicketsCost(e.target.value * adultTicketPrice);
-        setTotalTicketsPrice(adultTicketsCost + childTicketsCost)
-    }
-    const childTicketUpdate = (e) => {
-        setTotalTickets(adultTickets + childTickets);
-        setChildTickets(e.target.value);
-        setChildTicketsCost(e.target.value * childTicketPrice);
-        setTotalTicketsPrice(adultTicketsCost + childTicketsCost)
-    }
+  return (
+    <Form>
+      <Form.Group as={Col} controlId="adultTickets">
+        <Form.Label>Adult Tickets</Form.Label>
+        <Form.Control
+          value={adultTickets}
+          onChange={(e) => adultTicketUpdate(e)}
+          as="select"
+          defaultValue="Choose..."
+        >
+          <option>Choose...</option>
+          <option>1</option>
+          <option>2</option>
+          <option>3</option>
+          <option>4</option>
+          <option>5</option>
+          <option>6</option>
+          <option>7</option>
+          <option>8</option>
+          <option>9</option>
+        </Form.Control>
+      </Form.Group>
 
-    return (
-        <>
-            <p>Hello, ${firstName}</p>
-            <p>Movie: ${movieTitle}</p>
-            <p>Date: ${date}, Time: ${time}</p>
-            <Form>
-                <Form.Group as={Col} controlId="adultTickets">
-                    <Form.Label>Adult Tickets</Form.Label>
-                    <Form.Control value={adultTickets} onChange={(e) => adultTicketUpdate(e)} as="select" defaultValue="Choose...">
-                        <option>Choose...</option>
-                        <option>1</option>
-                        <option>2</option>
-                        <option>3</option>
-                        <option>4</option>
-                        <option>5</option>
-                        <option>6</option>
-                        <option>7</option>
-                        <option>8</option>
-                        <option>9</option>
-                    </Form.Control>
-                </Form.Group>
+      <Form.Group as={Col} controlId="childTickets">
+        <Form.Label>Child Tickets</Form.Label>
+        <Form.Control
+          value={childTickets}
+          onChange={(e) => childTicketUpdate(e)}
+          as="select"
+          defaultValue="Choose..."
+        >
+          <option>Choose...</option>
+          <option>1</option>
+          <option>2</option>
+          <option>3</option>
+          <option>4</option>
+          <option>5</option>
+          <option>6</option>
+          <option>7</option>
+          <option>8</option>
+          <option>9</option>
+        </Form.Control>
+      </Form.Group>
 
-                <Form.Group as={Col} controlId="childTickets">
-                    <Form.Label>Child Tickets</Form.Label>
-                    <Form.Control value={childTickets} onChange={(e) => childTicketUpdate(e)} as="select" defaultValue="Choose...">
-                        <option>Choose...</option>
-                        <option>1</option>
-                        <option>2</option>
-                        <option>3</option>
-                        <option>4</option>
-                        <option>5</option>
-                        <option>6</option>
-                        <option>7</option>
-                        <option>8</option>
-                        <option>9</option>
-                    </Form.Control>
-                </Form.Group>
+      <Form.Group as={Col} controlId="seniorTickets">
+        <Form.Label>Senior Tickets</Form.Label>
+        <Form.Control
+          value={seniorTickets}
+          onChange={(e) => seniorTicketUpdate(e)}
+          as="select"
+          defaultValue="Choose..."
+        >
+          <option>Choose...</option>
+          <option>1</option>
+          <option>2</option>
+          <option>3</option>
+          <option>4</option>
+          <option>5</option>
+          <option>6</option>
+          <option>7</option>
+          <option>8</option>
+          <option>9</option>
+        </Form.Control>
+      </Form.Group>
 
-                <p>Total tickets: ${totalTickets}</p>
-                <p>Total tickets cost: £${totalTicketsPrice}</p>
-                {rating>10? (<p>You will need to provide a valid form of ID when attempting to enter the screening</p>):(<p></p>)}
-                    <br />
-                <PaymentForm totalTicketsPrice={totalTicketsPrice} />
-            </Form>
-        </>
-    )
-}
+      <p>Total tickets: ${totalTickets}</p>
+      <p>Total tickets cost: £${totalTicketsPrice}</p>
+
+      <Button variant="primary" type="submit">
+        Submit
+      </Button>
+    </Form>
+  );
+};
 
 export default BookTickets;
