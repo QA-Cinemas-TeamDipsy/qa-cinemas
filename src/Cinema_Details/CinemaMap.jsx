cbfe1968f83056672922e6efd48e4dbdb52ed72b
NeelDQA <NDudhia@qa.com>
2021-06-08 11:48:37 +0100

    QAC 35 #comment ND:adding working location pin (api key needs to be changed)

--- src/Cinema_Details/CinemaMap.jsx
+++ src/Cinema_Details/CinemaMap.jsx
@@ -15,16 +15,16 @@ const CinemaMap = (location) => {
             <div className="map">
                 <div className="google-map">
                     <GoogleMapReact
-                        bootstrapURLKeys={{ key: ""}}
-                        defaultCenter={{lat: lat, lng: lng }}
-                        defaultZoom={7}
-                       
+                        bootstrapURLKeys={{ key: "AIzaSyCisLEBtwLnPP4J9jJPtkQ8G_WfrgWA434"}}
+                        defaultCenter={{lat: 51.3800776959354, lng: -0.1268782980874663}}
+                        defaultZoom={14}
+                    
                     >
-                        {/* <CinemaPin
-                            lat={lat}
-                            lng={lng}
+                        <CinemaPin
+                            lat={51.3800776959354}
+                            lng={-0.1268782980874663}
                             address={address}
-                        /> */}
+                        />
                     </GoogleMapReact>
                 </div>
 
