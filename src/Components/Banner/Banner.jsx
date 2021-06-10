6d6c1f0309f359ae2114b5f3ac80c57d3331c88d
Usman Sajid <usman230@hotmail.co.uk>
2021-06-10 10:22:14 +0100

    QAC-12 #comment updated home banner to feature movies

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -1,52 +1,91 @@
-import { Carousel, Image, Row, Col, Button, Container } from 'react-bootstrap';
+import { Carousel, Image, Row, Col, Button, Container } from "react-bootstrap";
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import axios from "axios";
 import "./Banner.css";
 
 const Banner = (props) => {
+  // const { movieProps } = props;
 
-    const { movies } = props;
+  const [movies, setMovies] = useState([]);
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [error, setError] = useState({});
 
-    return (
-        <Carousel>
-            {
-                movies.map((movie) => (
-                    <Carousel.Item>
-                        <Container>
-                            <Row>
-                                <Col sm>
-                                    <Image src={movie.Poster} />
-                                </Col>
-                                <Col sm >
-                                    <Carousel.Caption id="caption" >
-                                        <h3>{movie.Title}</h3>
-                                        <br />
-                                        <p>{movie.Plot}</p>
-                                    </Carousel.Caption>
+  useEffect(() => {
+    axios
+      .get("http://localhost:8080/api/movies/getAllFeatured")
+      .then((res) => {
+        setIsLoaded(true);
+        setMovies(res.data);
+      })
+      .catch((err) => {
+        setIsLoaded(true);
+        setError(err);
+      });
+  }, []);
+  console.log(movies);
 
-                                </Col>
-                                <Col className="align-items-center">
-                                <br/><br/><br/><br/><br/><br/><br/>
-                                    <Row className="justify-content-center">
-                                        <Button variant="outline-danger" >
-                                            View Info
-                                        </Button>
-                                    </Row>
-                                    <br/>
-                                    <Row className="justify-content-center ">
-                                        <Button variant="outline-danger" >
-                                            Book Tickets
-                                        </Button>
-                                    </Row>
+  return (
+    <Carousel>
+      {movies.map((movie) => (
+        <Carousel.Item>
+          <Container>
+            <Row>
+              <Col sm>
+                <Image src={movie.poster} />
+              </Col>
+              {/* <Carousel.Caption id="caption">
+                <h2>Now Showing</h2>
+              </Carousel.Caption> */}
 
-                                </Col>
-                            </Row>
-                        </Container>
-                    </Carousel.Item>
-
-                ))
-            }
-        </Carousel >
-    )
-
-}
+              <Col sm>
+                <Carousel.Caption id="caption">
+                  <h3>{movie.title}</h3>
+                  <br />
+                  <p>{movie.plot}</p>
+                </Carousel.Caption>
+              </Col>
+              <Col className="align-items-center">
+                <br />
+                <br />
+                <br />
+                <br />
+                <br />
+                <br />
+                <br />
+                <Row className="justify-content-center">
+                  <Link
+                    to={{
+                      pathname: `/movieinfo/${movie.imdbID}`,
+                      state: { movie },
+                    }}
+                  >
+                    <Button variant="outline-danger">View Info</Button>
+                  </Link>
+                </Row>
+                <br />
+                <Row className="justify-content-center ">
+                  <Link
+                    to={{
+                      pathname: "/MovieTimes",
+                      state: { movieTitle: movie.title, movieId: movie["_id"] },
+                    }}
+                  >
+                    <Button
+                      variant="outline-danger"
+                      disabled={movie.status.upcoming}
+                    >
+                      Go To Tickets
+                    </Button>
+                  </Link>
+                </Row>
+              </Col>
+            </Row>
+          </Container>
+        </Carousel.Item>
+      ))}
+    </Carousel>
+  );
+};
 
 export default Banner;
