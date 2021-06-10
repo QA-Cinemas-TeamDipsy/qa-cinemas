5da271cc47b992faa2afae056c05c7acb7ef2b11
Usman Sajid <usman230@hotmail.co.uk>
2021-06-10 10:25:10 +0100

    QAC-17 #comment implmented user login/logout buttons

--- src/Components/NavigationBar/NavigationBar.jsx
+++ src/Components/NavigationBar/NavigationBar.jsx
@@ -2,21 +2,26 @@ import React, { useRef } from "react";
 import { Nav, Navbar, Button, Form, FormControl, Image } from "react-bootstrap";
 import axios from "axios";
 import { NavLink } from "react-router-dom";
-import logo from "../static/Logo.png";
+import SearchedMovie from "../SearchedMovie/SearchedMovie.jsx";
+import UserRegistration from "../SignUp/UserRegistration";
+import UserLogin from "../SignUp/UserLogin";
+import Logo from "../static/Logo.png";
+import { useState, useEffect } from "react";
+import LoginButton from "./LoginButton";
+import LogoutButton from "./LogoutButton";
+import RegisterButton from "./RegisterButton";
 
-const NavigationBar = ({ setSearchedMovie }) => {
-  const movieRef = useRef("");
+const NavigationBar = ({ setSearchedMovie, user, setUser }) => {
+  const [showSearchModal, setShowSearchModal] = useState(false);
 
-  const { handleFilter, query } = props;
+  const [showRegModal, setShowRegModal] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    // console.log("eref", movieRef.current.value);
-    const { data } = await axios.get(
-      `http://www.omdbapi.com/?apikey=1fac6c28&t=${movieRef.current.value}`
-    );
-    // console.log("data", data);
-    setSearchedMovie(data);
+  const handleShowSearchModal = () => {
+    setShowSearchModal(true);
+  };
+  const handleShowRegModal = () => {
+    setShowRegModal(true);
   };
 
   return (
@@ -29,63 +34,57 @@ const NavigationBar = ({ setSearchedMovie }) => {
         expand="lg"
       >
         <Navbar.Brand to="/" href="/">
-          <Image src={logo} width={150} />
+          <Image src={Logo} alt="logo" width={150} />
         </Navbar.Brand>
-
         <Nav className="mr-auto">
           <Nav.Link href="/movies">What's On</Nav.Link>
+
           <Nav.Link href="/cinemas">Cinemas</Nav.Link>
+
+          <Nav.Link to="/DiscussionBoard" href="/DiscussionBoard">
+            Discussion Board
+          </Nav.Link>
+
           <Nav.Link href="/contactus">Contact Us</Nav.Link>
+
           <Nav.Link to="/about" href="/about">
             About Us
           </Nav.Link>
         </Nav>
 
-        <Nav className="mr-3">
+        {isLoggedIn ? (
+          <>
+            <h4>Hello {user.firstName} </h4>
+            <br />
+            <LogoutButton
+              updateIsLoggedIn={setIsLoggedIn}
+              updateUser={setUser}
+            />
+          </>
+        ) : (
+          <>
+            <LoginButton
+              updateIsLoggedIn={setIsLoggedIn}
+              updateUser={setUser}
+            />
+            <RegisterButton />
+          </>
+        )}
+        <Nav>
           <Button
-            variant="danger"
-            className="rounded-pill"
-            style={{ borderColor: "#343A40", border: "10px" }}
+            variant="outline-danger"
+            type="submit"
+            onClick={handleShowSearchModal}
           >
-            {" "}
-            <img src="https://img.icons8.com/android/24/000000/login-rounded-right.png" />
-            Login
-          </Button>{" "}
-          <Button
-            variant="danger"
-            className="rounded-pill"
-            style={{ borderColor: "#343A40", border: "10px" }}
-          >
-            <img src="https://img.icons8.com/small/32/000000/user.png" />
-            Sign Up
+            <img src="https://img.icons8.com/metro/26/000000/search.png" />
           </Button>
         </Nav>
-
-        <Nav>
-          <Form inline onSubmit={handleSubmit}>
-            {" "}
-            <input
-              className="mx-auto"
-              type="text"
-              name="filterBar"
-              id="filterBar"
-              value={query}
-              onInput={handleFilter}
-              placeholder="Filter characters on page..."
-            />
-            <FormControl
-              ref={movieRef}
-              type="text"
-              placeholder="Search"
-              className="mr-sm-2"
-            />
-            <Button variant="outline-danger" type="submit">
-              Search
-              {/* Needs to redirect to homepage */}
-            </Button>
-          </Form>
-        </Nav>
       </Navbar>
+
+      <SearchedMovie
+        showModal={showSearchModal}
+        updateState={setShowSearchModal}
+      />
     </>
   );
 };
