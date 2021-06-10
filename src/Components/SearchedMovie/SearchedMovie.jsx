37c6d103e4802f61f4febe48f7e0f7287e3dbe72
Usman Sajid <usman230@hotmail.co.uk>
2021-06-10 10:03:09 +0100

    QAC-7 #comment Implementation of search feature
    Co-authored-by: zaynejamilQA <zaynejamilQA@users.noreply.github.com>
    Co-authored-by: LamarrRD-QA <LamarrRD-QA@users.noreply.github.com>
    Co-authored-by: philipsephton-qa <philipsephton-qa@users.noreply.github.com>
    Co-authored-by: NeelDQA <NeelDQA@users.noreply.github.com>

--- src/Components/SearchedMovie/SearchedMovie.jsx
+++ src/Components/SearchedMovie/SearchedMovie.jsx
@@ -1,32 +1,107 @@
-import React from "react";
-import { Image, Col, Button } from "react-bootstrap";
+import React, { useRef, useState, useEffect } from "react";
+import axios from "axios";
+import { Image, Col, Button, Modal, Form, FormControl } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import "./ModalStyle.css";
 
-const SearchedMovie = ({ searchedMovie }) => {
+const SearchedMovie = ({ showModal, updateState, searchedMovie }) => {
+  const [movies, setMovies] = useState([]);
+  const [error, setError] = useState(null);
+  const [filterQuery, setFilterQuery] = useState("");
+  const [moviesToDisplay, setMoviesToDisplay] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:8080/api/movies/getAll")
+      .then((res) => {
+        console.log(res.data);
+        setMovies(res.data);
+      })
+      .catch((err) => {
+        setError(err);
+      });
+  }, []);
+
+  const handleClose = () => {
+    updateState(false);
+  };
+  const handleFilter = (e) => {
+    setFilterQuery(e.target.value.toLowerCase());
+  };
+
+  const showFilterList = () => {
+    if (filterQuery.length >= 3) {
+      return movies
+        .filter((movie) => movie.title.toLowerCase().includes(filterQuery))
+        .map((movie) => (
+          <li>
+            <Link
+              to={{
+                pathname: `/movieinfo/${movie.imdbID}`,
+                state: { movie },
+              }}
+              style={{
+                color: "white",
+                display: "inline-block",
+                marginBottom: "9px",
+              }}
+            >
+              {movie.title}
+            </Link>
+          </li>
+        ));
+    } else {
+      return <h4>{"No results found"}</h4>;
+    }
+  };
+  // const movieRef = useRef("");
+  // const handleSubmit = async (e) => {
+  //   e.preventDefault();
+  //   const { data } = await axios.get(
+  //     // `http://www.omdbapi.com/?apikey=1fac6c28&s=${movieRef.current.value}&plot=full`
+  //     `http://localhost:8080/api/movies/getAll`
+  //   );
+  //   console.log(data);
+  //   setMovies(data.Search);
+  // };
+
+  // useEffect(() => {
+  //   async function fetchData() {
+  //     const { data } = await axios.get(
+  //       `http://www.omdbapi.com/?apikey=1fac6c28&i=${id}`
+  //     );
+  //     setMovieInfo(data);
+  //   }
+  //   fetchData();
+  //   console.log(id);
+  // }, []);
+
   return (
-    <div className="container" style={{ marginTop: 30 }}>
-      <h4 style={{ margin: "20px 10px" }}>Search Result:</h4>
-      <Col sm={4} className="movie">
-        <Image className="posterImage-img" src={searchedMovie.Poster} />
-        <h3 style={{ margin: "10px 5px " }}>{searchedMovie.Title}</h3>
-        <Link to={`/movieinfo/${searchedMovie.imdbID}`}>
-          <Button
-            className="m-3"
-            variant="outline-danger"
-            movieData={searchedMovie}
-          >
-            View Info
-          </Button>
-        </Link>
+    <Modal
+      show={showModal}
+      onHide={handleClose}
+      keyboard={false}
+      scrollable={true}
+      style={{
+        minHeight: "100vh",
+        overflow: "scroll",
+      }}
+    >
+      <Modal.Header closeButton>
+        <Modal.Title>Search</Modal.Title>
+      </Modal.Header>
 
-        <Button variant="outline-danger" href="{}">
-          Book Ticket
-        </Button>
+      <Form>
+        <FormControl type="text" placeholder="Search" onChange={handleFilter} />
+      </Form>
 
-        <br />
-        <br />
+      <h4 style={{ margin: "20px 10px", color: "white" }}>Search Result:</h4>
+
+      <Col>
+        <ul id="searchResults">{showFilterList()}</ul>
       </Col>
-    </div>
+      <br />
+    </Modal>
   );
 };
 
