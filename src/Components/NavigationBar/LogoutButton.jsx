97f5af4adfffa4c505cff4d08a44ad69fbdb467c
Usman Sajid <usman230@hotmail.co.uk>
2021-06-10 10:15:23 +0100

    QAC-17  #comment Implementing user func

---
+++ src/Components/NavigationBar/LogoutButton.jsx
@@ -0,0 +1,24 @@
+import { Button } from "react-bootstrap";
+
+const LogoutButton = (props) => {
+  const { updateIsLoggedIn } = props;
+
+  const handleLogOut = () => {
+    updateIsLoggedIn(false);
+  };
+  return (
+    <>
+      <Button
+        variant="danger"
+        className="rounded-pill"
+        style={{ borderColor: "#343A40", border: "10pt" }}
+        onClick={handleLogOut}
+      >
+        <img src="https://img.icons8.com/small/32/000000/user.png" />
+        Log out
+      </Button>
+    </>
+  );
+};
+
+export default LogoutButton;
