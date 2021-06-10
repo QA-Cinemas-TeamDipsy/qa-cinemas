82b3ffbd828f412fb68c9336a86bad29a8902633
Usman Sajid <usman230@hotmail.co.uk>
2021-06-10 10:20:55 +0100

    QAC-12 #comment setup roots for getting movie info

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import AboutPage from "./Components/About/AboutPage";
 import NavigationBar from "./Components/NavigationBar/NavigationBar";
 import Movies from "./Components/MovieGallery/Movies";
 import ContactUs from "./Components/ContactUs/ContactUs";
+import MovieTimes from "./Components/Booking/MovieTimes";
+import BookTickets from "./Components/Booking/BookTickets";
+import PaymentForm from "./Components/Booking/PaymentForm";
 import Footer from "./Components/Footer/Footer";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import axios from "axios";
@@ -11,109 +14,85 @@ import axios from "axios";
 import "./App.css";
 import MovieInfo from "./Components/MovieInfo/MovieInfo";
 import SearchedMovie from "./Components/SearchedMovie/SearchedMovie";
+import DiscussionBoard from "./Components/DiscussionBoard/DiscussionBoard";
 
 export const MovieContext = createContext();
 
-function App() {
+const App = () => {
+  const [user, setUser] = useState({});
   const [currentMovies, setCurrentMovies] = useState([]);
   const [upcomingMovies, setUpcomingMovies] = useState([]);
 
   const [currentIsLoaded, setCurrentIsLoaded] = useState(false);
   const [futureIsLoaded, setFutureIsLoaded] = useState(false);
   const [err, setError] = useState({});
+
   const [searchedMovie, setSearchedMovie] = useState({});
 
-  useEffect(() => {
-    const MOVIES_API_URL = "http://www.omdbapi.com/?apikey=1fac6c28&";
-    const CURRENT_MOVIES = [
-      "tt7069210",
-      "tt8332922",
-      "tt3228774",
-      "tt5034838",
-      "tt5109280",
-      "tt10342730",
-      "tt9419056",
-      "tt3215824",
-      "tt7888964",
-    ];
-    const FUTURE_MOVIES = [
-      "tt3480822",
-      "tt5433138",
-      "tt0870154",
-      "tt8385148",
-      "tt3554046",
-      "tt9848626",
-    ];
+  // // console.log("movie", searchedMovie);
+  // useEffect(() => {
+  //   const loggedInUser = localStorage.getItem("user");
+  //   console.log(Object.entries(loggedInUser));
+  //   if (loggedInUser) {
+  //     const foundUser = JSON.parse(loggedInUser);
+  //     setUser(foundUser);
+  //   }
+  // }, []);
 
-    let currentMovieRequests = CURRENT_MOVIES.map((movie) => {
-      return axios.get(`${MOVIES_API_URL}i=${movie}`);
-    });
-
-    let upcomingMovieRequests = FUTURE_MOVIES.map((movie) => {
-      return axios.get(`${MOVIES_API_URL}i=${movie}`);
-    });
-
-    Promise.all(currentMovieRequests)
-      .then((responses) => {
-        // console.log(responses);
-        setCurrentIsLoaded(true);
-        setCurrentMovies(responses.map((x) => x.data));
-      })
-      .catch((err) => {
-        console.error(err.message);
-        setCurrentIsLoaded(true);
-        setError(err);
-      });
-
-    Promise.all(upcomingMovieRequests)
-      .then((responses) => {
-        // console.log(responses);
-        setFutureIsLoaded(true);
-        setUpcomingMovies(responses.map((x) => x.data));
-      })
-      .catch((err) => {
-        console.error(err.message);
-        setFutureIsLoaded(true);
-        setError(err);
-      });
-  }, []);
-  console.log("movie", searchedMovie);
   return (
     <>
-      {currentMovies.length > 0 && upcomingMovies.length > 0 && (
-        <MovieContext.Provider value={[...currentMovies, ...upcomingMovies]}>
-          <Router>
-            <NavigationBar setSearchedMovie={setSearchedMovie} />
-            <br />
-            <Switch>
-              <Route exact path="/">
-                <HomePage />
-                {searchedMovie && (
-                  <SearchedMovie searchedMovie={searchedMovie} />
-                )}
-              </Route>
+      <Router>
+        <Route>
+          <NavigationBar
+            searchedMovie={searchedMovie}
+            user={user}
+            setUser={setUser}
+          />
+        </Route>
 
-              <Route exact path="/ContactUs" component={ContactUs}></Route>
+        <br />
 
-              <Route exact path="/movies">
-                <Movies
-                  currentMovies={currentMovies}
-                  upcomingMovies={upcomingMovies}
-                />
-              </Route>
-              <Route exact path="/movieinfo/:id" component={MovieInfo}></Route>
+        <Switch>
+          <Route exact path="/">
+            <HomePage />
+          </Route>
 
-              <Route exact path="/about">
-                <AboutPage />
-              </Route>
-            </Switch>
-            <br />
-            <Footer />
-          </Router>
-        </MovieContext.Provider>
-      )}
+          <Route exact path="/ContactUs" component={ContactUs} />
+
+          <Route exact path="/movies">
+            <Movies
+              currentMovies={currentMovies}
+              upcomingMovies={upcomingMovies}
+            />
+          </Route>
+          <Route exact path="/movieinfo/:id" component={MovieInfo}></Route>
+          <Route
+            exact
+            path="/MovieTimes"
+            render={(props) => (
+              <MovieTimes {...props} cinemaId={user.cinemaID} />
+            )}
+          ></Route>
+          <Route exact path="/BookTickets">
+            <BookTickets />
+          </Route>
+          <Route exact path="/Payments">
+            <PaymentForm />
+          </Route>
+
+          <Route exact path="/about">
+            <AboutPage />
+          </Route>
+
+          <Route exact path="/DiscussionBoard">
+            <DiscussionBoard />
+          </Route>
+        </Switch>
+        <br />
+        <Footer />
+      </Router>
     </>
   );
-}
+};
 
 export default App;
